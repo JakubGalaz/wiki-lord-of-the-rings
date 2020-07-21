5fd1b3d793f2523193cb9acee0eceb6118b0050f
kubixes <kubixes@interia.pl>
2020-07-21 15:04:51 +0200

    search bar start

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,20 @@
+import { LocationsService } from './../services/locations.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  documents;
 
-  constructor() { }
+  constructor(private locationsService: LocationsService) {}
 
   ngOnInit(): void {
+    this.documents = [...this.locationsService.documents];
+    console.log('documents: ' + this.documents);
   }
 
+  search(e) {}
 }
