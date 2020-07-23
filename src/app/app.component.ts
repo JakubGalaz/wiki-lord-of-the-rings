74e3f672027a2ae4cd1d9f091f8fba7de6ab10cf
kubixes <kubixes@interia.pl>
2020-07-23 10:49:38 +0200

    update json files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'wiki';
+
+  constructor(private route: Router) {}
+
+  goToHome(): void {
+    this.route.navigateByUrl('/home');
+  }
 }
